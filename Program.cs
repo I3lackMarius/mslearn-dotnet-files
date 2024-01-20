419de2cfa5bc8f999531f3af77c2cb8c94addd26
I3lackMarius <1993.marius@gmail.com>
2024-01-20 17:05:53 +0100

    ex3-create folders and files from code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using System.IO;
 using System.Collections.Generic;
 
+/** 
+* * programma per scorrere il contenuto di una cartella e trovare 
+* * eventuali file sales.json al suo interno o in una sottocartella.
+*/
+/*
 var salesFiles = FindFiles("stores");
+string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
 foreach (var file in salesFiles)
 {
@@ -18,6 +24,83 @@ IEnumerable<string> FindFiles(string folderName)
     {
         // The file name will contain the full path, so only check the end of it
         if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+*/
+//Console.WriteLine($"Current path: {Directory.GetCurrentDirectory()}");
+//Console.WriteLine($"Special folder Documents path: {docPath}");
+//Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+//Console.WriteLine(Path.Combine("stores", "201")); // outputs: stores\201
+//Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
+/*
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+FileInfo info = new FileInfo(fileName);
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+*/
+
+
+/** 
+* * programma per scorrere il contenuto di una cartella con ricerca dimanica delle path e trovare 
+* * eventuali file con estensione .json al suo interno o in una sottocartella.
+*/
+/*
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+*/
+
+/** 
+* * programma per creare files e directory
+*/
+
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+var salesFiles = FindFiles(storesDirectory);
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
